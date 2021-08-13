fc8e753d90b48e508a6ef1e521c6e9fad375a147
~!@#$%^&*()_+}{ <88084445+emrahselek@users.noreply.github.com>
2021-08-12 20:35:00 -0600

    Update Emrah.java

--- src/Emrah.java
+++ src/Emrah.java
@@ -1,6 +1,8 @@
 public class Emrah {
     public static void main(String[] args) {
         System.out.println("emrah");
+        System.out.println("emrah");
+        System.out.println("emrah");
 
 
 
